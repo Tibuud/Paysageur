645b7e26e19fa346172ff0d9b5b2f2168310aec2
Thibaud Bugier <thibaud.bugier@gmail.com>
2018-03-24 21:03:32 +0100

    Mise en forme et modification de la page Archive products

--- wp-content/themes/paysageur/woocommerce/archive-product.php
+++ wp-content/themes/paysageur/woocommerce/archive-product.php
@@ -1,111 +1,130 @@
 <?php
-/**
- * The Template for displaying product archives, including the main shop page which is a post type archive
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.3.0
- */
-
 if (! defined('ABSPATH')) {
     exit;
 }
 
-get_header('shop');
+get_header(); ?>
+<main class="container-fluid conteneur-principal">
+        <section class='sous-menu navbar navbar-expand-lg navbar-light'>
+                <div  id='hook-edition' class="d-flex justify-content-around">
+                   <a class='' href="#hook-produits">Produits</a>
+                   <a class='' href="#hook-points_de_vente">Points de Vente</a>
 
-/**
- * Hook: woocommerce_before_main_content.
- *
- * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
- * @hooked woocommerce_breadcrumb - 20
- * @hooked WC_Structured_Data::generate_website_data() - 30
- */
-do_action('woocommerce_before_main_content');
+               </div>
 
-?>
-<header class="woocommerce-products-header">
-	<?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
+        </section>
+        <section class='produits-page-boutique'> <!-- debut section produit de la page revue -->
+            <h3 id='hook-produits'>Produits</h3>
+            <div class="text-center d-flex justify-content-around flex-wrap">
+            <?php
+            if (have_posts()) {
+                woocommerce_product_loop_start();
 
-	<?php
-    /**
-     * Hook: woocommerce_archive_description.
-     *
-     * @hooked woocommerce_taxonomy_archive_description - 10
-     * @hooked woocommerce_product_archive_description - 10
-     */
-    do_action('woocommerce_archive_description');
-    ?>
-</header>
+                if (wc_get_loop_prop('total')) {
+                    while (have_posts()) {
+                        the_post();
+                        do_action('woocommerce_shop_loop');
+                        wc_get_template_part('content', 'product');
+                    }
+                }
+                woocommerce_product_loop_end();
+                do_action('woocommerce_after_shop_loop');
+            } else {
+                do_action('woocommerce_no_products_found');
+            } ?>
+            </div>
+        </section> <!-- fin section produit de la page revue -->
+        <section class='section_points-de-vente'>
+            <h3 id='hook-points_de_vente'>Points de vente</h3>
 <?php
 
-if (have_posts()) {
+ $terms_pays = get_terms(array(
+    'taxonomy' => 'pays',
+    'orderby' => 'name',
+    'order' => 'ASC',
+    'hide_empty' => false,
 
-    /**
-     * Hook: woocommerce_before_shop_loop.
-     *
-     * @hooked wc_print_notices - 10
-     * @hooked woocommerce_result_count - 20
-     * @hooked woocommerce_catalog_ordering - 30
-     */
-    do_action('woocommerce_before_shop_loop');
+ ));
 
-    woocommerce_product_loop_start();
+ $terms_ville = get_terms(array(
+     'taxonomy' => 'ville',
+     'orderby' => 'name',
+     'order' => 'ASC',
+     'hide_empty' => false,
+ ));
 
-    if (wc_get_loop_prop('total')) {
-        while (have_posts()) {
-            the_post();
+ foreach ($terms_pays as $value) {
+     $pays_slug = $value->slug;
+     $pays_name = $value->name;
 
-            /**
-             * Hook: woocommerce_shop_loop.
-             *
-             * @hooked WC_Structured_Data::generate_product_data() - 10
-             */
-            do_action('woocommerce_shop_loop');
+     foreach ($terms_ville as $value) {
+         $ville_slug = $value->slug;
+         $ville_name = $value->name;
+         $args = array(
+             'post_type' => 'points_de_vente',
+             'tax_query' => array(
+                 array(
+                     'taxonomy' => 'ville',
+                     'field'    => 'slug',
+                     'terms'    => $ville_slug,
+                 ),
+                 array(
+                     'taxonomy' => 'pays',
+                     'field'    => 'slug',
+                     'terms'    => $pays_slug,
+                 ),
+             ),
+          );
 
-            wc_get_template_part('content', 'product');
-        }
-    }
+         $the_query = new WP_Query($args);
+         if (isset($liste_pays)) {
+             if (($the_query->have_posts()) && !in_array($pays_slug, $liste_pays)) {
+                 echo "<h4>" . $pays_name . "</h4>";
+             }
+         } elseif ($the_query->have_posts()) {
+             echo "<h4>" . $pays_name . "</h4>";
+         }
 
-    woocommerce_product_loop_end();
+         if (isset($liste_villes)) {
+             if (($the_query->have_posts()) && !in_array($ville_slug, $liste_villes)) {
+                 echo "<p class='ville'>" . $ville_name . "</p>";
+                 echo "<div class='d-flex flex-wrap justify-content-start'>";
+             }
+         } elseif ($the_query->have_posts()) {
+             echo "<p class='ville'>" . $ville_name . "</p>";
+             echo "<div class='d-flex flex-wrap justify-content-start'>";
+         }
 
-    /**
-     * Hook: woocommerce_after_shop_loop.
-     *
-     * @hooked woocommerce_pagination - 10
-     */
-    do_action('woocommerce_after_shop_loop');
-} else {
-    /**
-     * Hook: woocommerce_no_products_found.
-     *
-     * @hooked wc_no_products_found - 10
-     */
-    do_action('woocommerce_no_products_found');
-}
+         if ($the_query->have_posts()) {
+             while ($the_query->have_posts()) {
+                 $the_query->the_post(); ?>
+                 <div class="d-flex flex-wrap justify-content-start">
+                     <div class='point-de-vente'>
+                         <p><?php the_field("nom_de_la_boutique"); ?></p>
+                         <p><?php the_field("adresse"); ?></p>
+                         <p><?php the_field("code_postal"); ?> <?php echo $ville_name ?></p>
+                         <p><?php the_field("numero"); ?></p>
+                         <p><a href="#"><?php the_field("lien"); ?></a><?php the_field("nom_lien"); ?></p>
+                     </div>
+                 </div>
+             <?php
+             }
 
-/**
- * Hook: woocommerce_after_main_content.
- *
- * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
- */
-do_action('woocommerce_after_main_content');
+             if (isset($liste_villes)) {
+                 if (($the_query->have_posts()) && !in_array($ville_slug, $liste_villes)) {
+                     echo "</div>";
+                 }
+             } elseif ($the_query->have_posts()) {
+                 echo "</div>";
+             }
 
-/**
- * Hook: woocommerce_sidebar.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action('woocommerce_sidebar');
-
-get_footer('shop');
+             $liste_villes[] = $ville_slug;
+             $liste_pays[] = $pays_slug;
+             wp_reset_postdata();
+         }
+     }
+ } ?>
+ </section>
+ </main>
+ <?php
+get_footer();
