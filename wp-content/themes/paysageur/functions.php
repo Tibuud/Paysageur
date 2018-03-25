38d628742fe444edef8fd68e696ef7eb4c3e1dcf
Thibaud Bugier <thibaud.bugier@gmail.com>
2018-03-25 16:27:31 +0200

    Des fichiers de custom du starter sont remis en place pour la stabilité

--- wp-content/themes/paysageur/functions.php
+++ wp-content/themes/paysageur/functions.php
@@ -147,7 +147,19 @@ function paysageur_scripts()
 }
 add_action('wp_enqueue_scripts', 'paysageur_scripts');
 
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
 
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Load WooCommerce compatibility file.
+ */
 if (class_exists('WooCommerce')) {
     require get_template_directory() . '/inc/woocommerce.php';
 }
